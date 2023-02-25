3d33470718e6e87c42b08ad2a90199c35c85a73b
Dias Daribayev <fission.frames-0s@icloud.com>
2023-02-25 12:30:43 +0600

    In models.py in EmailField changed verbose_name from incorrect "john_doe@mail.com" to just "email"

--- source/guest_book/models.py
+++ source/guest_book/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
         (BLOCKED, 'Blocked'),
     )
     author = models.CharField(max_length=40, null=False, blank=False, default='Author', verbose_name='Author')
-    email = models.EmailField(max_length=40, null=True, blank=True, default='john_doe@mail.com', verbose_name='john_doe@mail.com')
+    email = models.EmailField(max_length=40, null=True, blank=True, default='john_doe@mail.com', verbose_name='email')
     text = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Text')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=ACTIVE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Crated at')
